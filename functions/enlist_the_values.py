e0bad581f13c2426d5b52f3a51668b7e944a1aa5
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2021-12-21 22:28:24 +0100

    rearaning the functions to make them indepandant

--- functions/enlist_the_values.py
+++ functions/enlist_the_values.py
@@ -6,15 +6,18 @@ wb = load_workbook("excel-files\Listes d'Exam S5 EG 2018.2019 pour Etud.xlsx") #
 
 ws = wb.active
 
-def iterate_table(row_list):
+def enlist_the_values(row):
+    arr = []
     column_end = len(ws['A'])
     print(column_end)
     column = 0
     while(column <= column_end):
         column = column + 1
-        for x in row_list:
+        for x in row:
             exp = f'{x}{column}'
             if ws[exp].value == None :
              continue
             else:
                 print(ws[exp].value)
+                arr.append(ws[exp].value)
+    return arr
